5298e2a50e34e914a723a727dd67ca3110331bb8
Patryk Hirny <pathirny@googlemail.com>
2023-09-15 11:27:45 +0100

    added game start function which gets user choice and computer choice

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+let playerChoice = "";
+let computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+
+function gameStart(){
+    /*(let gameModeChoice = prompt("Choose Game mode. Best of 3 or 5?");
+    console.log(gameModeChoice)
+    */
+
+    playerChoice = prompt("Please choose: Rock, paper or scissors");
+    console.log(playerChoice.toLowerCase());
+    
+        computerChoice = Math.floor(Math.random() * 3 + 1);
+        console.log(computerChoice);
+        if (computerChoice === 1){
+            console.log("Computer picks Scissors");
+        } else if (computerChoice === 2){
+            console.log("Computer picks Paper");
+        } else if (computerChoice === 3){
+            console.log("Computer picks Rock");
+        };
+
+        if (playerChoice === computerChoice){
+            console.log("Same choice try")
+        }
+}
+
+gameStart();
