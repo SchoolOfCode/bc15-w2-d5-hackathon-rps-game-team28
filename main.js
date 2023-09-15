5bd9ed7c10f9eb26aff1b86a39a85dcaf229e3ab
Patryk Hirny <pathirny@googlemail.com>
2023-09-15 11:41:14 +0100

    same choice comparison working

--- main.js
+++ main.js
@@ -8,22 +8,22 @@ function gameStart(){
     console.log(gameModeChoice)
     */
 
-    playerChoice = prompt("Please choose: Rock, paper or scissors");
+    playerChoice = prompt("Please choose: 1.Rock, 2.paper or 3.scissors");
     console.log(playerChoice.toLowerCase());
     
-        computerChoice = Math.floor(Math.random() * 3 + 1);
-        console.log(computerChoice);
-        if (computerChoice === 1){
-            console.log("Computer picks Scissors");
-        } else if (computerChoice === 2){
-            console.log("Computer picks Paper");
-        } else if (computerChoice === 3){
-            console.log("Computer picks Rock");
-        };
+    computerChoice = Math.floor(Math.random() * 3 + 1);
+    console.log(computerChoice);
+    if (computerChoice === 1){
+        console.log("Computer picks Rock");
+    } else if (computerChoice === 2){
+        console.log("Computer picks paper");
+    } else if (computerChoice === 3){
+        console.log("Computer picks rock");
+    };
 
-        if (playerChoice === computerChoice){
-            console.log("Same choice try again")
-        } else if(playerChoice === "rock" && computerChoice === "scissors"){
+    if (playerChoice == computerChoice){
+        console.log("Same choice try")
+    } else if(playerChoice === "rock" && computerChoice === "scissors"){
             console.log("You won that round")
         } else if(playerChoice === "scissors" && computerChoice === "paper"){
             console.log("You won that round")
