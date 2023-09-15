6b1fa43232b63a0f2f5d032bc29436e47acad67f
Patryk Hirny <pathirny@googlemail.com>
2023-09-15 15:52:37 +0100

    final touches again

--- main.js
+++ main.js
@@ -78,11 +78,16 @@ function gameStart(){
     }}  
     
     if (gameModeChoice == false){
+        playerName = prompt("What is player 1's name?");
+            alert(`Hello ${playerName}`);
+        playerTwoName = prompt("What is player 2's name?");
+            alert(`Hello ${playerTwoName}`);    
+
         while (playerScore || secondPlayerScore !== 3){
-            playerChoice = prompt("Please choose Player 1: Rock, Paper or Scissors");
+            playerChoice = prompt(`${playerName} Please choose: Rock, Paper or Scissors`);
             console.log(playerChoice.toLowerCase());
             // above is to take the players choice
-            secondPlayerChoice = prompt("Please choose Player 2: Rock, Paper or Scissors");
+            secondPlayerChoice = prompt(`${playerTwoName} Please choose Player 2: Rock, Paper or Scissors`);
             console.log(secondPlayerChoice.toLowerCase());
 /*  Ensures right spelling of inputs
             if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors"){
